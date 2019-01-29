924120dd60b6d826679fdd0de8ecd7ae866976e3
ericcchiu <eric.chn.chiu@gmail.com>
2019-01-29 15:48:27 -0600

    (route) Setup and connect events route for search and fetching all events.

--- server/index.js
+++ server/index.js
@@ -4,8 +4,22 @@ const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
 const port = 3000;
 
-server.use(middlewares)
-server.use(router)
+server.use(middlewares);
+server.use(router);
+// server.use('/api', router);
+
+server.get('/events', (req, res) => {
+  res.jsonp(req.query);
+});
+
+server.get('/events' + '?q=' + req.params.query, (req, res) => {
+  res.jsonp(req.query);
+});
+
+// server.get('/events?&_limit=20', (req, res) => {
+//   res.jsonp(req.query);
+// });
+
 server.listen(port, () => {
   console.log(`JSON Server is running on port ${port}`);
 });
