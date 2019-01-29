fefaab5e7d53f5a27dae705a7b7de4dcf5cdd2bb
ericcchiu <eric.chn.chiu@gmail.com>
2019-01-29 14:42:49 -0600

    (setup) Connect json server to serve static file. Configured webpack to bundle files with babel.

---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+module.exports = {
+  context: `${__dirname}/client/src`,
+  entry: './index.js',
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-react', '@babel/preset-env'],
+            plugins: ['@babel/plugin-proposal-class-properties']
+          }
+        }
+      },
+      {
+        test: /\.css$/,
+        loader: [
+          'style-loader',
+          {
+            loader: 'css-loader'
+          },
+        ],
+      },
+      {
+        test: /\.(png|jpg|gif)$/i,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 8192
+            }
+          }
+        ]
+      }
+
+    ]
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx']
+  },
+  output: {
+    path: `${__dirname}/public`,
+    filename: 'bundle.js'
+  },
+};
